76e387ec61f15d2fb81532aee5d8c69bfd13ef09
lenamarochkina <marochkina.lena@gmail.com>
2023-11-23 18:40:51 +0100

    feat: add more test

--- src/test.sh
+++ src/test.sh
@@ -3,10 +3,11 @@
 # Test your program with multiple graphs
 
 # List of graph files
-graph_files=("graph_0.txt" "graph_1.txt" "graph_2.txt" "graph_3.txt" "graph_4.txt" "graph_5.txt"
+graph_files=("graph_0.txt" "graph_1.txt" "graph_2.txt" "graph_3.txt" "graph_4_Error_Test.txt" "graph_5_Error_Test.txt" "graph_6.txt" "graph_7.txt" "graph_8_Error_Test.txt" "graph_9.txt"
             "graph_Cycles.txt"
-            "graph_Duplicate_Edges.txt" "graph_Sparse_Graph.txt"
-            "graph_Negative_Weight.txt" "graph_Wrong_Weight.txt" "graph_Negative_Weight_2.txt" )
+            "graph_Duplicate_Edges_Error_Test.txt" "graph_Sparse_Graph.txt"
+            "graph_Negative_Weight.txt" "graph_Wrong_Weight_Error_Test.txt" "graph_Wrong_Weight_2_Error_Test.txt" "graph_Negative_Weight_2.txt"
+            "graph_Wrong_Structure_Error_Test.txt")
 
 # Iterate over each graph file
 for file in "${graph_files[@]}"; do
